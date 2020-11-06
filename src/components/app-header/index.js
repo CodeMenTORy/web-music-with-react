017656f0ba588f284f88466b2a8f4a089637bde2
tory <tory1327643851@gmail.com>
2020-11-06 10:43:57 +0800

    顶部导航栏组件左部分开发完成

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,12 +1,42 @@
 import React, { memo } from 'react';
+
+import { headerLinks } from '@/common/local-data';
+
 import { NavLink } from 'react-router-dom';
+import { HeaderLeft, HeaderRight, HeaderWrapper } from './style';
 
 export default memo(function AppHeader() {
+  const showSelectItem = (item, index) => {
+    if (index < 3) {
+      return (
+        <NavLink to={item.link} exact>
+          {item.title}
+          <i className="sprite_01 icon"></i>
+        </NavLink>
+      );
+    } else {
+      return <a href={item.link}>{item.title}</a>;
+    }
+  };
+
   return (
-    <div>
-      <NavLink to="/">发现音乐</NavLink>
-      <NavLink to="/mine">我的音乐</NavLink>
-      <NavLink to="/friend">朋友</NavLink>
-    </div>
+    <HeaderWrapper>
+      <div className="content wrap-v1">
+        <HeaderLeft>
+          <a href="#/discover" className="logo sprite_01"></a>
+          <div className="select-list">
+            {headerLinks.map((item, index) => {
+              return (
+                <div key={item.title} className="select-item">
+                  {showSelectItem(item, index)}
+                </div>
+              );
+            })}
+          </div>
+        </HeaderLeft>
+        <HeaderRight>Right</HeaderRight>
+      </div>
+      <div className="divider"></div>
+    </HeaderWrapper>
   );
 });
