f758d6ce8d56eaebe1bc99e5bed9804919ca5361
tory <tory1327643851@gmail.com>
2020-11-20 21:58:09 +0800

    新碟上架组件开发完成

--- src/pages/discover/children-pages/recommend/children-cpns/new-album/index.js
+++ src/pages/discover/children-pages/recommend/children-cpns/new-album/index.js
@@ -1,10 +1,64 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useRef } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+
+import { Carousel } from 'antd';
+
 import ThemeHeaderRCM from '../../../../../../components/theme-header-rcmd';
+import { getNewAlbumAction } from '../../store/actionsCreators';
+import AlbumCover from '@/components/album-cover';
+
+import { AlbumWrapper } from './style';
 
 export default memo(function NewAlbum() {
+  const { newAlbums } = useSelector(
+    (state) => ({
+      newAlbums: state.getIn(['recommend', 'newAlbums']),
+    }),
+    shallowEqual,
+  );
+  const dispatch = useDispatch();
+
+  const pageRef = useRef();
+  useEffect(() => {
+    dispatch(getNewAlbumAction(10));
+  }, [dispatch]);
+
   return (
-    <div>
+    <AlbumWrapper>
       <ThemeHeaderRCM title="新碟上架" />
-    </div>
+      <div className="content">
+        <button
+          className="arrow arrow-left sprite_02"
+          onClick={(e) => pageRef.current.prev()}
+        ></button>
+        <div className="album">
+          <Carousel dots={false} ref={pageRef}>
+            {[0, 1].map((item) => {
+              return (
+                <div key={item} className="page">
+                  {newAlbums.slice(item * 5, (item + 1) * 5).map((iten) => {
+                    return (
+                      <AlbumCover
+                        key={iten.id}
+                        info={iten}
+                        size={100}
+                        width={118}
+                        bgp="-570px"
+                      >
+                        {iten.name}
+                      </AlbumCover>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </Carousel>
+        </div>
+        <button
+          className="arrow arrow-right sprite_02"
+          onClick={(e) => pageRef.current.next()}
+        ></button>
+      </div>
+    </AlbumWrapper>
   );
 });
--- src/pages/discover/children-pages/recommend/store/actionsCreators.js
+++ src/pages/discover/children-pages/recommend/store/actionsCreators.js
@@ -1,6 +1,7 @@
 import * as actionTypes from './constants';
 
 import { getTopBanners, getHotRecommends } from '@/services/recommend';
+import { getNewAlbums } from '../../../../../services/recommend';
 
 const changeTopBannerAction = (res) => ({
   type: actionTypes.CHANGE_TOP_BANNERS,
@@ -25,6 +26,20 @@ export const getHotRecommendAction = (limit) => {
   return (dispatch) => {
     getHotRecommends(limit).then((res) => {
       dispatch(changeHotRecommendsAction(res));
+    });
+  };
+};
+
+const changeNewAlbumAction = (res) => ({
+  type: actionTypes.CHANGE_NEW_ALBUMS,
+  newAlbums: res.albums,
+});
+
+// 获取新歌推荐
+export const getNewAlbumAction = (limit) => {
+  return (dispatch) => {
+    getNewAlbums(limit).then((res) => {
+      dispatch(changeNewAlbumAction(res));
     });
   };
 };
