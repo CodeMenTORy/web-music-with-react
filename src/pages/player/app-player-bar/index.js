fdebdc160735cbeee33f8ff3d261667e50e67c96
tory <tory1327643851@gmail.com>
2020-11-24 21:51:24 +0800

    播放功能开发完成

--- src/pages/player/app-player-bar/index.js
+++ src/pages/player/app-player-bar/index.js
@@ -1,49 +1,119 @@
-import React, { memo, useEffect } from 'react';
+import React, { memo, useCallback, useEffect, useRef, useState } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 
 import { Slider } from 'antd';
-
 import { Control, Operator, PlaybarWrapper, PlayInfo } from './style';
 
-import { useDispatch } from 'react-redux';
 import { getSongDetailAction } from '../store/actionCreator';
 
+import { getSizeImage, formatDate, getPlaySong } from '@/utils/format-utils';
+
 export default memo(function AppPlayerBar() {
+  // props and state
+  const [currentTime, setCurrentTime] = useState(0);
+  const [progress, setProgress] = useState(0);
+  const [isChanging, setIsChanging] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  // ruduer hooks
+  const { currentSong } = useSelector(
+    (state) => ({
+      currentSong: state.getIn(['player', 'currentSong']),
+    }),
+    shallowEqual,
+  );
   const dispatch = useDispatch();
 
+  // other hooks
   useEffect(() => {
     dispatch(getSongDetailAction(167876));
   }, [dispatch]);
+  useEffect(() => {
+    audioRef.current.src = getPlaySong(currentSong.id);
+  }, [currentSong]);
+  const audioRef = useRef();
+
+  // other handle
+  const picUrl = (currentSong.al && currentSong.al.picUrl) || '';
+  const singerName = (currentSong.ar && currentSong.ar[0].name) || 'undefined';
+  const duration = currentSong.dt || 0;
+  const showDuration = formatDate(duration, 'mm:ss');
+  const ShowCurrentTime = formatDate(currentTime, 'mm:ss');
+
+  // handle function
+  // 播放按钮点击
+  const playMusic = useCallback(() => {
+    isPlaying ? audioRef.current.pause() : audioRef.current.play();
+    setIsPlaying(!isPlaying);
+  }, [isPlaying]);
+  // 更新播放进度时间
+  const timeUpdate = (e) => {
+    if (!isChanging) {
+      setCurrentTime(e.target.currentTime * 1000);
+      setProgress((currentTime / duration) * 100);
+    }
+  };
+  // 监听进度条滑动
+  const sliderChange = useCallback(
+    (value) => {
+      setIsChanging(true);
+      const currentTime = (value / 100) * duration;
+      setCurrentTime(currentTime);
+      setProgress(value);
+    },
+    [duration],
+  );
+  // 监听进度条滑动后停止
+  const sliderAfterChange = useCallback(
+    (value) => {
+      const currentTime = ((value / 100) * duration) / 1000;
+      audioRef.current.currentTime = currentTime;
+      setCurrentTime(currentTime * 1000);
+      setIsChanging(false);
+
+      if (!isPlaying) {
+        playMusic();
+      }
+    },
+    [duration, isPlaying, playMusic],
+  );
 
   return (
     <PlaybarWrapper className="sprite_player">
       <div className="content wrap-v2">
-        <Control>
+        <Control isPlaying={isPlaying}>
           <button className="sprite_player prev"></button>
-          <button className="sprite_player play"></button>
+          <button
+            className="sprite_player play"
+            onClick={(e) => playMusic()}
+          ></button>
           <button className="sprite_player next"></button>
         </Control>
         <PlayInfo>
           <div className="image">
             <a href="/todo">
-              <img
-                src="https://p1.music.126.net/mNe3491FsGAjuuPkSH9WLA==/109951163405538210.jpg?param=34y34"
-                alt=""
-              />
+              <img src={getSizeImage(picUrl, 35)} alt="" />
             </a>
           </div>
           <div className="info">
             <div className="song">
-              <span className="song-name">红豆</span>
+              <span className="song-name">{currentSong.name}</span>
               <a href="/todo" className="singer-name">
-                王菲
+                {singerName}
               </a>
             </div>
             <div className="progress">
-              <Slider defaultValue={30} tooltipVisible={false} />
+              <Slider
+                defaultValue={30}
+                tooltipVisible={false}
+                value={progress}
+                onChange={sliderChange}
+                onAfterChange={sliderAfterChange}
+              />
               <div className="time">
-                <span className="now-time">02:20</span>
+                <span className="now-time">{ShowCurrentTime}</span>
                 <span className="divider">/</span>
-                <span className="duration">02:20</span>
+                <span className="duration">{showDuration}</span>
               </div>
             </div>
           </div>
@@ -60,6 +130,7 @@ export default memo(function AppPlayerBar() {
           </div>
         </Operator>
       </div>
+      <audio ref={audioRef} onTimeUpdate={timeUpdate} />
     </PlaybarWrapper>
   );
 });
